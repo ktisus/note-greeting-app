37153bed4c77f20dd4fdce0012337166438d5045
ktisus <140302003@svuca.edu>
2019-03-19 17:34:18 -0700

    first commit

---
+++ capital.js
@@ -0,0 +1,7 @@
+exports.capitalEngland = function () {
+    console.log ('London')
+};
+
+exports.capitalSpain = function() {
+    console.log('Madrid')
+};
---
+++ main.js
@@ -0,0 +1,8 @@
+const greetings = require('./greeting.js');
+const capitals = require('./capital.js');
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+capitals.capitalEngland();
